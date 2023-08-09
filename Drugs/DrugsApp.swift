26b9954279556db1e3750096cb2aa0c6c16b6080
Shadi187 <101983035+Shadi187@users.noreply.github.com>
2023-08-09 15:18:46 +0300

    Initial Commit

---
+++ Drugs/DrugsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrugsApp.swift
+//  Drugs
+//
+//  Created by boshFbt on 09/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct DrugsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
